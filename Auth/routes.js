f7b63affa13d432fb3ed92db0327a68be84d7d52
Zealll <eriznis@gmail.com>
2019-08-22 22:27:16 -0400

    register made functional

--- Auth/routes.js
+++ Auth/routes.js
@@ -7,6 +7,22 @@ const helpers = require('./helpers.js')
 const {generateToken} = require('./middleware.js')
 
 
+router.post('/register', async (req,res) => {
+    let user = req.body
+    const hash = bcrypt.hashSync(user.password, 4)
+    user.password = hash
+
+
+    try {
+        const registered = await helpers.register(user, user.type)
+
+        res.status(201).json(registered)
+    } catch (err) {
+        res.status(500).json({message: 'Something went wrong when registering user'})
+    }
+})
+
+
 router.post('/login', (req, res) => {
     const {username, password} = req.body
 
@@ -16,9 +32,11 @@ router.post('/login', (req, res) => {
         if(!user) {
             res.status(404).json({message: 'Can\'t find an User with the specified username'})
         } else {
+            if(brcrypt.compareSync(password, user.password)) {
+                const token = generateToken(user)
+                res.status(201).json({message: `Logged In! Your ID is ${user.id}`, token})
+            }
             
-            const token = generateToken(user)
-            res.status(201).json({message: `Logged In! Your ID is ${user.id}`, token})
         }
         
     })
